5f70335f31e290e8d8101be3449010f9d84f2d71
Valentin <rivettivalentin@gmail.com>
2021-11-24 20:02:03 -0300

    Fixed App.js readability and login redirection functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
 import {GlobalStyle} from './GlobalStyle'
 
@@ -9,38 +9,28 @@ import { Details } from './pages/Details'
 import { Liked } from './pages/Liked'
 import { Profile } from './pages/Profile'
 import { NotRegistered } from './pages/NotRegistered'
-import Context from './Context'
+import { Context } from './Context'
 
 
 export const App = () => {
 
+  const { isAuth } = useContext(Context)
+
   return (
     <>
       <GlobalStyle />
-        <Router>
+      <Router>
         <Layout>
           <Routes>
             <Route path='/' element={ <Home /> } />
             <Route path='/pet/:id' element={ <Home /> } />
             <Route path='/detail=:id' element={ <Details />} />
+            <Route path='/liked' element={ isAuth ? <Liked /> : <Navigate replace to='/login'/> } />
+            <Route path='/profile' element={ isAuth ? <Profile /> : <Navigate replace to='/login'/> } />
+            <Route path='/login' element={isAuth ? <Navigate replace to='/' /> : <NotRegistered />} />
           </Routes>
-          <Context.Consumer>
-          {
-            ({isAuth}) =>
-              isAuth ?
-              <Routes>
-                <Route path='/liked' element={<Liked />} />
-                <Route path='/profile' element={<Profile/>} />
-              </Routes>
-              :
-              <Routes>
-                <Route path='/liked' element={<NotRegistered />} />
-                <Route path='/profile' element={<NotRegistered/>} />
-              </Routes>
-          }
-          </Context.Consumer>
-          </Layout>
-        </Router>
+        </Layout>
+      </Router>
     </>
   )
 }
