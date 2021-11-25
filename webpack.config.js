b534a1acad0dbeb991764975ab17f69a211b29a9
Valentin <rivettivalentin@gmail.com>
2021-11-25 12:51:13 -0300

    Added pwa manifest (Not working tho)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const html = require('html-webpack-plugin')
+const WebpackManifest = require('webpack-pwa-manifest')
+const path = require('path')
 
 
 module.exports = {
@@ -17,7 +19,21 @@ module.exports = {
     ]
   },
   plugins: [
-    new html({template: './public/index.html'})
+    new html({template: './public/index.html'}),
+    new WebpackManifest({
+      name: 'Petbook, tu app de fotos de mascotas',
+      shortname: 'Petbook 😸',
+      start_url: '/index.html',
+      description: 'Con petbook puedes encontrar animales tiernos',
+      background_color: '#18191a',
+      theme_color: '#242526',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    })
   ],
   devServer: {
     historyApiFallback: true
